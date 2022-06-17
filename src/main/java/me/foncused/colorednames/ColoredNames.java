87e2973dba52a16ff6559880f6ed749c8fa9f5d7
foncused <foncused@users.noreply.github.com>
2022-06-17 13:23:23 -0400

    Update ColoredNames.java

--- src/main/java/me/foncused/colorednames/ColoredNames.java
+++ src/main/java/me/foncused/colorednames/ColoredNames.java
@@ -7,7 +7,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class ColoredNames extends JavaPlugin {
 
-	public static final String PREFIX = "[SBNameTags] ";
+	public static final String PREFIX = "[ColoredNames] ";
 
 	private ConfigManager cm;
 
